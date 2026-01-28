df7ab943df4001c4adab38bbf964f71d94d02f8d
angel_anchaluisa <angel_anchaluisa@trabajo.gob.ec>
2026-01-28 10:00:05 -0500

    Iniciando el ejercicio #3

---
+++ 03-indecision-app/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Mi primera app en Vue</h1>
+    <MyCounter :initialValue=8 />
+    <br><hr>
+    <MyCounter :initial-value="5" />
+</template>
+
+<script lang="ts" setup>
+    import MyCounter from './components/MyCounter.vue';
+</script>
---
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h3>Counter: {{ counter }}</h3>
+        <h3>Square: {{ cuadrado }}</h3>
+        <div>
+            <button v-on:click="minus">-1</button>
+            <button @click="plus">+1</button>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+    import { computed, ref } from 'vue';
+
+    interface Props {
+        initialValue: number;
+        value?: string;
+    }
+
+    const props = defineProps<Props>();
+
+    const counter = ref(props.initialValue);
+    const cuadrado = computed( () => counter.value * counter.value );
+    
+    const plus = () => counter.value++;
+    const minus = () => counter.value--;
+</script>
+
+<style scoped>
+h3 {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
