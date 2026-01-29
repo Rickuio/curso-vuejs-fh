2f8a0858c83731b4fd31f7b5b5771607a950a585
angel_anchaluisa <angel_anchaluisa@trabajo.gob.ec>
2026-01-29 10:12:43 -0500

    Creacion de MyCounterScript

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -3,8 +3,11 @@
     <MyCounter :initialValue=8 />
     <br><hr>
     <MyCounter :initial-value="5" />
+    <br><hr>
+    <MyCounterScript :valor="2" />
 </template>
 
 <script lang="ts" setup>
     import MyCounter from './components/MyCounter.vue';
+    import MyCounterScript from './components/MyCounterScript.vue';
 </script>
---
+++ 03-indecision-app/src/components/MyCounterScript.vue
@@ -0,0 +1,41 @@
+<template>
+    <section>
+        <h3>Counter: {{ counter }}</h3>
+        <h3>Square: {{ square }}</h3>
+        <div>
+            <button v-on:click="counter--">-1</button>
+            <button @click="counter++">+1</button>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue';
+
+
+export default defineComponent({
+
+    props: {
+        valor: { type: Number, required: true }, 
+    },
+    setup(props) {
+        
+        const counter = ref(props.valor);
+        const cuadrado = computed(() => counter.value * counter.value);
+        
+        return {
+            counter: counter,
+            square: cuadrado,
+        }
+    
+    }
+   
+});
+
+</script>
+
+<style scoped>
+h3 {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
